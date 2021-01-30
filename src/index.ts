50fe36fa3f213520753cbfe15c6add3ebf3b7395
Simon Ensslen <simon.ensslen@griesser.ch>
2021-01-30 17:19:41 +0100

    remove namespaces and add additional imports

--- src/index.ts
+++ src/index.ts
@@ -9,44 +9,43 @@ import { ICameraConnection } from './CameraConnection/ICameraConnection';
 import { IVideoMixer } from './VideoMixer/IVideoMixer';
 import { IHmi } from './Hmi/IHmi';
 import { IConfigurationStructure } from './Configuration/IConfigurationStructure';
+import { IConfig } from './GenericFactory/IConfig';
 
-export namespace Cgf {
-    export namespace CameraControl {
-        export namespace Main {
-            export class Core {
-                private _camFactory = new CameraConnectionFactory();
-                private _mixerFactory = new VideomixerFactory();
-                private _hmiFactory = new HmiFactory();
+export class Core {
+    private _camFactory = new CameraConnectionFactory();
+    private _mixerFactory = new VideomixerFactory();
+    private _hmiFactory = new HmiFactory();
 
-                bootstrap(logger: ILogger, config: IConfigurationStructure) {
-                    this.cameraBuilderAdd(new PtzLancCameraBuilder(logger));
-                    this.videoMixerBuilderAdd(new AtemBuilder(logger, this._camFactory));
+    bootstrap(logger: ILogger, config: IConfigurationStructure) {
+        this.cameraBuilderAdd(new PtzLancCameraBuilder(logger));
+        this.videoMixerBuilderAdd(new AtemBuilder(logger, this._camFactory));
 
-                    for (let cam of config.cams) {
-                        this._camFactory.parseConfig(cam);
-                    }
+        for (let cam of config.cams) {
+            this._camFactory.parseConfig(cam);
+        }
 
-                    for (let videoMixer of config.videoMixers) {
-                        this._mixerFactory.parseConfig(videoMixer);
-                    }
+        for (let videoMixer of config.videoMixers) {
+            this._mixerFactory.parseConfig(videoMixer);
+        }
 
-                    for (let hmi of config.interfaces) {
-                        this._hmiFactory.parseConfig(hmi);
-                    }
-                }
-
-                cameraBuilderAdd(builder: IBuilder<ICameraConnection>) {
-                    this._camFactory.builderAdd(builder);
-                }
-
-                videoMixerBuilderAdd(builder: IBuilder<IVideoMixer>) {
-                    this._mixerFactory.builderAdd(builder);
-                }
-
-                interfaceBuilderAdd(builder: IBuilder<IHmi>) {
-                    this._hmiFactory.builderAdd(builder);
-                }
-            }
+        for (let hmi of config.interfaces) {
+            this._hmiFactory.parseConfig(hmi);
         }
     }
+
+    cameraBuilderAdd(builder: IBuilder<ICameraConnection>) {
+        this._camFactory.builderAdd(builder);
+    }
+
+    videoMixerBuilderAdd(builder: IBuilder<IVideoMixer>) {
+        this._mixerFactory.builderAdd(builder);
+    }
+
+    interfaceBuilderAdd(builder: IBuilder<IHmi>) {
+        this._hmiFactory.builderAdd(builder);
+    }
 }
+
+export { IHmi };
+export { IBuilder };
+export { IConfig };
