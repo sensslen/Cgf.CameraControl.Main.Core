86588e6ff96a6722a9debae8e2a48afb949f6e19
Simon Ensslen <simon.ensslen@griesser.ch>
2021-03-09 21:35:30 +0100

    readd rumblepad export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { F310Builder } from './logitech/F310/F310Builder';
 import { F710Builder } from './logitech/F710/F710Builder';
+import { Rumblepad2Builder } from './logitech/Rumblepad2/Rumblepad2Builder';
 
 export { F310Builder };
 export { F710Builder };
+export { Rumblepad2Builder };
