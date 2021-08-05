838851b1760e3a54c5229b37d4ce6aed7bf461de
Simon Ensslen <simon.ensslen@griesser.ch>
2021-08-05 22:34:03 +0200

    remove idea (notify when a macro is executed). This does not work....

--- src/VideoMixer/IMacroRun.ts
+++
@@ -1,3 +0,0 @@
-export interface IMacroRun {
-    macroRunUpdate(macro: number): void;
-}
--- src/VideoMixer/IVideoMixer.ts
+++ src/VideoMixer/IVideoMixer.ts
@@ -2,7 +2,6 @@ import { EventEmitter } from 'events';
 import { IConnection } from '../GenericFactory/IConnection';
 import { IDisposable } from '../GenericFactory/IDisposable';
 import { IImageSelectionChange } from './IImageSelectionChange';
-import { IMacroRun } from './IMacroRun';
 import { ISubscription } from '../GenericFactory/ISubscription';
 import StrictEventEmitter from 'strict-event-emitter-types';
 
@@ -13,12 +12,6 @@ export interface IVideoMixer extends ISubscription<IConnection>, IDisposable {
      * Get the event emitter that allows to register for image selection changes
      */
     imageSelectionChangeGet(): StrictEventEmitter<EventEmitter, IImageSelectionChange>;
-    /**
-     * Get the event emitter that allows to register for a macro run event.
-     * This event might also be fired if a macro is run on a different MixEffectBlock
-     * on the same device
-     */
-    macroRunEmitterGet(): StrictEventEmitter<EventEmitter, IMacroRun>;
 
     cut(): void;
     auto(): void;
