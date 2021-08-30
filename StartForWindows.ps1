fc6094dece86653a04bd6ddfb0cf1824efa45795
Simon Ensslen <simon.ensslen@griesser.ch>
2021-08-30 21:37:53 +0200

    Add startup scripts for mac and windows

---
+++ StartForWindows.ps1
@@ -0,0 +1,15 @@
+﻿$ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+
+Add-Type -AssemblyName System.Windows.Forms
+
+$ConfigFileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    Filter = 'Configuration (*.json)|*.json'
+}
+
+$dialogResult = $ConfigFileBrowser.ShowDialog()
+
+if ($dialogResult -eq [System.Windows.Forms.DialogResult]::OK) {
+    $fileName=$ConfigFileBrowser.FileName
+    $command = "node `"$ScriptPath/dist/index.js`" --config `"$fileName`""
+    Invoke-Expression $command
+}
